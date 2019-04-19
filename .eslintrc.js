88eaf11cef7652650afeb634de87b795fa35a9d0
Sébastien Vidal <svidal1@free.fr>
2019-04-19 11:14:31 +0200

    replace asynchronous-eventemitter by EventEmitter

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,6 @@ module.exports = {
         "clearInterval": true,
         "clearTimeout": true,
         "describe": true,
-        "EventEmitter": true,
         "it": true,
         "jQuery": true,
         "Promise": true,
