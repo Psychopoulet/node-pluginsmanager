facd2d1392de35510e5dceb0b3607d40449e14c2
Sébastien Vidal <svidal1@free.fr>
2019-07-09 15:42:56 +0200

    improve directory management

--- test/4_01_constructor.js
+++ test/4_01_constructor.js
@@ -18,22 +18,24 @@ describe("pluginsmanager / constructor", () => {
 		const pluginsManager = new PluginsManager();
 
 		// private
-		assert.strictEqual(pluginsManager._beforeLoadAll, null, "Generated plugin is not as expected");
+		assert.strictEqual(pluginsManager._beforeInitAll, null, "Generated plugin is not as expected");
 		assert.deepStrictEqual(pluginsManager._orderedDirectoriesBaseNames, [], "Generated plugin is not as expected");
 		assert.strictEqual(pluginsManager._maxListeners, 0, "Generated plugin is not as expected");
 
 		// public
-		assert.strictEqual(pluginsManager.directory, join(__dirname, "..", "lib", "plugins"), "Generated plugin is not as expected");
+		assert.strictEqual(pluginsManager.directory, "", "Generated plugin is not as expected");
 		assert.deepStrictEqual(pluginsManager.plugins, [], "Generated plugin is not as expected");
 
 	});
 
 	it("should test given directory", () => {
 
-		const pluginsManager = new PluginsManager(join(__dirname, "plugins2"));
+		const pluginsManager = new PluginsManager({
+			"directory": join(__dirname, "plugins2")
+		});
 
 		// private
-		assert.strictEqual(pluginsManager._beforeLoadAll, null, "Generated plugin is not as expected");
+		assert.strictEqual(pluginsManager._beforeInitAll, null, "Generated plugin is not as expected");
 		assert.deepStrictEqual(pluginsManager._orderedDirectoriesBaseNames, [], "Generated plugin is not as expected");
 		assert.strictEqual(pluginsManager._maxListeners, 0, "Generated plugin is not as expected");
 
