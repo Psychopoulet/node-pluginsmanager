4ec57baf9e04418d2faad667938e84766bb6a97e
Sébastien Vidal <svidal1@free.fr>
2019-08-22 16:10:12 +0200

    improve update tests

--- test/4_09_update_via_github.js
+++ test/4_09_update_via_github.js
@@ -3,26 +3,26 @@
 // deps
 
 	// natives
-	const { join } = require("path");
+	const { basename, join } = require("path");
 	const { strictEqual } = require("assert");
+	const { Orchestrator } = require("node-pluginsmanager-plugin");
 
 	// externals
-	const { isDirectoryProm, rmdirpProm } = require("node-promfs");
+	const { rmdirpProm } = require("node-promfs");
 
 	// locals
+	const copyPlugin = require(join(__dirname, "utils", "copyPlugin.js"));
 	const PluginsManager = require(join(__dirname, "..", "lib", "main.js"));
+	const isDirectory = require(join(__dirname, "..", "lib", "checkers", "isDirectory.js"));
 
 // const
 
 	const MAX_TIMOUT = 30 * 1000;
 
-	const GITHUB_USER = "Psychopoulet";
-	const GITHUB_REPO = "node-pluginsmanager-plugin-test";
-
 	const PLUGINS_DIRECTORY = join(__dirname, "plugins");
 
-		const GOOD_PLUGIN_DIRECTORY = join(PLUGINS_DIRECTORY, GITHUB_REPO);
-			const GOOD_PLUGIN_MODULES_DIRECTORY = join(GOOD_PLUGIN_DIRECTORY, "node_modules");
+		const TEST_PLUGIN_DIRECTORY = join(PLUGINS_DIRECTORY, "TestUpdate");
+			const TEST_PLUGIN_MODULES_DIRECTORY = join(TEST_PLUGIN_DIRECTORY, "node_modules");
 
 	const EVENTS_DATA = "test";
 
@@ -32,20 +32,6 @@ describe("pluginsmanager / update via github", () => {
 
 	const pluginsManager = new PluginsManager({
 		"directory": PLUGINS_DIRECTORY
-	});
-
-	before(() => {
-		return pluginsManager.loadAll();
-	});
-
-	after(() => {
-
-		return pluginsManager.releaseAll().then(() => {
-			return pluginsManager.destroyAll();
-		}).then(() => {
-			return rmdirpProm(GOOD_PLUGIN_DIRECTORY);
-		});
-
 	});
 
 	describe("params", () => {
@@ -80,11 +66,95 @@ describe("pluginsmanager / update via github", () => {
 
 		});
 
+		it("should test update with missing repo", (done) => {
+
+			const orchestrator = new Orchestrator();
+
+				orchestrator.name = basename(TEST_PLUGIN_DIRECTORY);
+
+			pluginsManager.updateViaGithub(orchestrator).then(() => {
+				done(new Error("tests does not generate error"));
+			}).catch((err) => {
+
+				strictEqual(typeof err, "object", "Generated error is not an object");
+				strictEqual(err instanceof ReferenceError, true, "Generated error is not an instance of Error");
+
+				done();
+
+			});
+
+		});
+
+		it("should test update with not loaded plugin", (done) => {
+
+			const orchestrator = new Orchestrator();
+
+				orchestrator.name = basename(TEST_PLUGIN_DIRECTORY);
+				orchestrator.github = "whatever";
+
+			pluginsManager.updateViaGithub(orchestrator).then(() => {
+				done(new Error("tests does not generate error"));
+			}).catch((err) => {
+
+				strictEqual(typeof err, "object", "Generated error is not an object");
+				strictEqual(err instanceof Error, true, "Generated error is not an instance of Error");
+
+				done();
+
+			});
+
+		});
+
 	});
 
 	describe("execute", () => {
 
-		it("should test update plugins and dependances", () => {
+		afterEach(() => {
+
+			pluginsManager.removeAllListeners();
+
+			return pluginsManager.releaseAll().then(() => {
+				return pluginsManager.destroyAll();
+			}).then(() => {
+				return rmdirpProm(TEST_PLUGIN_DIRECTORY);
+			}).then(() => {
+
+				return new Promise((resolve) => {
+					setTimeout(resolve, 500);
+				});
+
+			});
+
+		});
+
+		it("should test update plugins and dependancies with \"github\" parameter", () => {
+
+			const pluginName = basename(TEST_PLUGIN_DIRECTORY);
+
+			return copyPlugin(PLUGINS_DIRECTORY, "TestGoodPlugin", pluginName, {
+				"name": pluginName,
+				"github": "git://github.com/Psychopoulet/node-pluginsmanager-plugin-test"
+			}).then(() => {
+
+				return pluginsManager.loadAll();
+
+			}).then(() => {
+
+				strictEqual(pluginsManager.plugins.length, 3, "Distant plugin not installed");
+
+				return pluginsManager.updateViaGithub(pluginsManager.plugins.filter((plugin) => {
+					return pluginName === plugin.name;
+				})[0] || null, EVENTS_DATA);
+
+			}).then(() => {
+
+				return isDirectory("update/execute", TEST_PLUGIN_MODULES_DIRECTORY);
+
+			});
+
+		}).timeout(MAX_TIMOUT);
+
+		it("should test update plugins and dependancies with \"repository\" parameter", () => {
 
 			pluginsManager.on("updated", (plugin, data) => {
 
@@ -95,27 +165,56 @@ describe("pluginsmanager / update via github", () => {
 
 			});
 
-			return pluginsManager.installViaGithub(GITHUB_USER, GITHUB_REPO).then(() => {
+			const pluginName = basename(TEST_PLUGIN_DIRECTORY);
 
-				return isDirectoryProm(GOOD_PLUGIN_MODULES_DIRECTORY).then((exists) => {
-					return exists ? Promise.resolve() : Promise.reject(new Error("There is no npm udpate performed"));
-				});
-
+			return copyPlugin(PLUGINS_DIRECTORY, "TestGoodPlugin", pluginName, {
+				"name": pluginName,
+				"repository": "git://github.com/Psychopoulet/node-pluginsmanager-plugin-test"
 			}).then(() => {
 
-				return rmdirpProm(GOOD_PLUGIN_MODULES_DIRECTORY);
+				return pluginsManager.loadAll();
 
 			}).then(() => {
 
 				strictEqual(pluginsManager.plugins.length, 3, "Distant plugin not installed");
 
-				return pluginsManager.updateViaGithub(pluginsManager.plugins[2], EVENTS_DATA);
+				return pluginsManager.updateViaGithub(pluginsManager.plugins.filter((plugin) => {
+					return pluginName === plugin.name;
+				})[0] || null, EVENTS_DATA);
 
 			}).then(() => {
 
-				return isDirectoryProm(GOOD_PLUGIN_MODULES_DIRECTORY).then((exists) => {
-					return exists ? Promise.resolve() : Promise.reject(new Error("There is no npm udpate performed"));
-				});
+				return isDirectory("update/execute", TEST_PLUGIN_MODULES_DIRECTORY);
+
+			});
+
+		}).timeout(MAX_TIMOUT);
+
+		it("should test update plugins and dependancies with \"repository.url\" parameter", () => {
+
+			const pluginName = basename(TEST_PLUGIN_DIRECTORY);
+
+			return copyPlugin(PLUGINS_DIRECTORY, "TestGoodPlugin", pluginName, {
+				"name": pluginName,
+				"repository": {
+					"type": "git",
+					"url": "git://github.com/Psychopoulet/node-pluginsmanager-plugin-test"
+				}
+			}).then(() => {
+
+				return pluginsManager.loadAll();
+
+			}).then(() => {
+
+				strictEqual(pluginsManager.plugins.length, 3, "Distant plugin not installed");
+
+				return pluginsManager.updateViaGithub(pluginsManager.plugins.filter((plugin) => {
+					return pluginName === plugin.name;
+				})[0] || null, EVENTS_DATA);
+
+			}).then(() => {
+
+				return isDirectory("update/execute", TEST_PLUGIN_MODULES_DIRECTORY);
 
 			});
 
