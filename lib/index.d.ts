10e06e5bfd554e0cfb5838bf4cac3863bf5800bd
Seb - physical <svidal1@free.fr>
2022-05-31 11:19:58 +0200

    improve typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,16 +1,16 @@
 /// <reference types="node" />
-/// <reference types="node-pluginsmanager-plugin" />
 
 declare module "node-pluginsmanager" {
 
 	import { EventEmitter } from "events";
 	import { Orchestrator } from "node-pluginsmanager-plugin";
 	import { Server as WebSocketServer } from "ws";
+	import { Server as SocketIOServer } from "socket.io";
 
 	interface iPluginManagerOptions {
-		"directory": string; // plugins location. default : join(homedir(), "node-pluginsmanager-plugins")
-		"externalRessourcesDirectory": string; // external resources locations (sqlite, files, cache, etc...). default : join(homedir(), "node-pluginsmanager-resources")
-		"logger": Function | null;
+		"directory"?: string; // plugins location. default : join(homedir(), "node-pluginsmanager-plugins")
+		"externalRessourcesDirectory"?: string; // external resources locations (sqlite, files, cache, etc...). default : join(homedir(), "node-pluginsmanager-resources")
+		"logger"?: Function | null;
 	}
 
 	class PluginManager extends EventEmitter {
@@ -19,8 +19,8 @@ declare module "node-pluginsmanager" {
 
 			// protected
 
-				protected _beforeLoadAll: Function | null;
-				protected _beforeInitAll: Function | null;
+				protected _beforeLoadAll: (data?: any) => Promise<void> | void | null;
+				protected _beforeInitAll: (data?: any) => Promise<void> | void | null;
 
 				protected _orderedPluginsNames: Array<string>;
 
@@ -54,17 +54,17 @@ declare module "node-pluginsmanager" {
 				// network
 
 				public appMiddleware(req: Request, res: Response, next: Function): void;
-				public socketMiddleware(server: WebSocketServer): void;
+				public socketMiddleware(server: WebSocketServer | SocketIOServer): void;
 
 				// load / destroy
 
-				public beforeLoadAll(callback: () => Promise<any>): Promise<void>;
+				public beforeLoadAll(callback: (data?: any) => Promise<void> | void): Promise<void>;
 				public loadAll(data?: any): Promise<void>;
 				public destroyAll(data?: any): Promise<void>;
 
 				// init / release
 
-				public beforeInitAll(callback: () => Promise<any>): Promise<void>;
+				public beforeInitAll(callback: (data?: any) => Promise<void> | void): Promise<void>;
 				public initAll(data?: any): Promise<void>;
 				public releaseAll(data?: any): Promise<void>;
 
--- test/typescript/compilation.ts
+++ test/typescript/compilation.ts
@@ -1,21 +1,126 @@
 /// <reference path="../../lib/index.d.ts" />
 
-import * as PluginManager from "../../lib/main.js";
+// deps
 
-console.log(PluginManager);
+	// natives
+	import { join } from "path"
+	import { homedir } from "os"
 
-const manager = new PluginManager();
+	// locals
+	import PluginManager = require("node-pluginsmanager");
 
-console.log(manager);
+// consts
 
-console.log(manager.getPluginsNames());
-
-manager.setOrder([ "1", "2" ]).then(() => {
-
-	return manager.beforeInitAll(() => {
-		return manager.releaseAll();
+	const manager: PluginManager = new PluginManager({
+		"directory": join(homedir(), "MySoftware", "plugins")
 	});
 
-}).then(() => {
-	return manager.initAll();
-});
+// module
+
+try {
+
+	console.log(PluginManager);
+	console.log(manager);
+
+	manager
+
+		// error
+		.on("error", (msg): void => {
+			console.log("--- [event/error] '" + msg.error + "' ---");
+		})
+
+		// load
+		.on("loading", (pluginName): void => {
+			console.log("--- [event/loading] '" + pluginName + "' loading ---");
+		}).on("loaded", (plugin): void => {
+			console.log("--- [event/loaded] '" + plugin.name + "' (v" + plugin.version + ") loaded ---");
+		}).on("allloaded", (): void => {
+			console.log("--- [event/allloaded] all plugins allloaded ---");
+		})
+
+		// init
+		.on("initializing", (plugin): void => {
+			console.log("--- [event/initializing] '" + plugin.name + "' (v" + plugin.version + ") initialized ---");
+		}).on("initialized", (plugin): void => {
+			console.log("--- [event/initialized] '" + plugin.name + "' (v" + plugin.version + ") initialized ---");
+		}).on("allinitialized", (): void => {
+			console.log("--- [event/initialized] all plugins initialized ---");
+		})
+
+		// release
+		.on("released", (pluginName): void => {
+			console.log("--- [event/released] '" + pluginName + " released ---");
+		}).on("allreleased", (): void => {
+			console.log("--- [event/released] all plugins released ---");
+		})
+
+		// destroy
+		.on("destroyed", (pluginName): void => {
+			console.log("--- [event/destroyed] '" + pluginName + " destroyed ---");
+		}).on("alldestroyed", (): void => {
+			console.log("--- [event/destroyed] all plugins destroyed ---");
+		})
+
+		// write
+		.on("installed", (plugin): void => {
+			console.log("--- [event/installed] '" + plugin.name + "' (v" + plugin.version + ") installed ---");
+		}).on("updated", (plugin): void => {
+			console.log("--- [event/updated] '" + plugin.name + "' (v" + plugin.version + ") updated ---");
+		}).on("uninstalled", (pluginName): void => {
+			console.log("--- [event/uninstalled] '" + pluginName + "' uninstalled ---");
+		});
+
+	manager.setOrder([ "1", "2" ]).then((): Promise<void> => {
+
+		return manager.beforeInitAll((): Promise<void> => {
+			return manager.releaseAll(); // just to check beforeInitAll
+		});
+
+	}).then((): Promise<void> => {
+		return manager.initAll();
+	}).then((): void => {
+
+		console.log("all plugins initialized");
+		console.log(manager.getPluginsNames());
+
+		/*
+		manager.installViaGithub(
+			<account>,
+			<plugin>,
+			<optional data to pass to the 'install' && 'init' plugins methods>
+		).then((plugin) => {
+			console.log(plugin.name + ' installed & initialized');
+		}).catch((err) => {
+			console.log(err);
+		});
+
+		manager.updateViaGithub( // use "github"'s plugin data if exists
+			<plugin>,
+			<optional data to pass to the 'release', 'update', && 'init' plugins methods>
+		).then((plugin) => {
+			console.log(plugin.name + ' updated & initialized');
+		}).catch((err) => {
+			console.log(err);
+		});
+
+		manager.uninstall(
+			<plugin>,
+			<optional data to pass to the 'release' && 'uninstall' plugins methods>
+		).then((pluginName) => {
+			console.log(pluginName + ' removed');
+		}).catch((err) => {
+			console.log(err);
+		});
+		*/
+
+	});
+
+}
+catch (e) {
+
+	console.error(e);
+
+	process.exitCode = 1;
+	process.exit(1);
+
+}
