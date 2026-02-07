a2e87868a8e8c9fe58e42f27c05c1b3df683e55a
Sébastien VIDAL <meona@live.fr>
2026-02-07 02:04:17 +0100

    fix cmd output error catch

--- lib/src/cmd/cmd.ts
+++ lib/src/cmd/cmd.ts
@@ -26,19 +26,32 @@ export default function cmd (directory: string, command: string, params: string[
 
         return new Promise((resolve: () => void, reject: (err: Error) => void): void => {
 
-            let result: string = "";
+            let errMsg: string = "";
+            let outMsg: string = "";
             const mySpawn: ChildProcessWithoutNullStreams = spawn(command, params, {
                 "cwd": directory,
                 "windowsHide": true,
                 "shell": true
             }).on("error", (err: Error): void => {
-                result += stdToString(err);
+                errMsg += stdToString(err);
             }).on("close", (code: number): void => {
-                return code ? reject(new Error(result)) : resolve();
+
+                if (0 < errMsg.length) {
+                    return code ? reject(new Error(errMsg)) : resolve();
+                }
+
+                return code ? reject(new Error(outMsg)) : resolve();
+
             });
 
-            mySpawn.stderr.on("data", (msg: any): void => {
-                result += stdToString(msg);
+            mySpawn.stdout.setEncoding("utf8");
+            mySpawn.stdout.on("data", (msg: string): void => {
+                outMsg += stdToString(msg);
+            });
+
+            mySpawn.stderr.setEncoding("utf8");
+            mySpawn.stderr.on("data", (msg: string): void => {
+                errMsg += stdToString(msg);
             });
 
         });
