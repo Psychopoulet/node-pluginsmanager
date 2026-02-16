03b08e3c634a192fc61d83390e5c2930ee0e8646
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-02-16 18:21:26 +0100

    use flat eslint config

--- .eslintrc-back.js
+++ .eslintrc-back.js
@@ -1,3 +1,17 @@
-module.exports = {
-    "extends": [ "plugin:eslint-plugin-personnallinter/ts-back" ]
-};
+// deps
+
+    // externals
+    const { defineConfig } = require("eslint/config");
+    const personnallinter = require("eslint-plugin-personnallinter");
+
+// module
+
+module.exports = defineConfig({
+    "plugins": {
+        personnallinter
+    },
+    "extends": [ personnallinter.configs["ts-back"] ],
+    "rules": {
+        "@typescript-eslint/no-require-imports": "warn"
+    }
+});
